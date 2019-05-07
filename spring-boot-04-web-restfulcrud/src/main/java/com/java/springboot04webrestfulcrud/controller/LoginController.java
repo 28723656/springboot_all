d5f7cef8debdb2d57867bb74a2c1e7d09264d8aa
xiarui <28723656@qq.com>
2019-05-07 22:52:03 +0800

    update:1.使用th:replace重用   2.高亮选中的部分

--- spring-boot-04-web-restfulcrud/src/main/java/com/java/springboot04webrestfulcrud/controller/LoginController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/java/springboot04webrestfulcrud/controller/LoginController.java
@@ -3,11 +3,9 @@ package com.java.springboot04webrestfulcrud.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -16,15 +14,15 @@ public class LoginController {
     @PostMapping(value = "/user/login")
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password,
-                        Map<String,Object> map,
-                        HttpSession session){
-        if(!StringUtils.isEmpty(username) && "123456".equals(password)){
-            session.setAttribute("loginUser",username);
+                        Map<String, Object> map,
+                        HttpSession session) {
+        if (!StringUtils.isEmpty(username) && "123456".equals(password)) {
+            session.setAttribute("loginUser", username);
             //登陆成功
             return "redirect:/main.html";
-        }else{
+        } else {
             //登录失败
-            map.put("msg","登陆失败");
+            map.put("msg", "登陆失败");
             return "/login";
         }
 
