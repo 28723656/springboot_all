38030a487bd9c9e33c64906cfdbca1b91f380625
xiarui <28723656@qq.com>
2019-05-09 22:06:29 +0800

    add:springboot整合jdbc

---
+++ spring-boot-06-data-jdbc/src/main/java/com/java/springboot/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.java.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 风往西边吹丶
+ * @create 2019-05-09 21:28
+ */
+@Controller
+public class HelloController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public  List<Map<String, Object>> map(){
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from department");
+        return list;
+    }
+
+}
